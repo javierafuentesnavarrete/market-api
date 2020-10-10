6c43fd71cb3f4f4e33d539453edfcc2d3ff73d0c
chriscoulbeck <chriscoulbeck7@gmail.com>
2020-10-10 23:20:52 +1300

    Redactored product schema to require properties

--- models/Product.js
+++ models/Product.js
@@ -1,16 +1,11 @@
 const mongoose = require("mongoose");
 
 const ProductsSchema = new mongoose.Schema({
-    title: {type: String},
-    price:{type:String},
-    date:{type:String},
-    active:{type:String},
-    location:{type:String},
-    username: {type:String},
-    userrating:{type:String},
-    details:{type:String},
-    description: {type: String},
-    category:{type:String}
+    title: {type: String, required: true},
+    price:{type: String, required: true},
+    username: {type: String, required: true},
+    details:{type: String, required: true},
+    description: {type: String, required: true},
 
 })
 
